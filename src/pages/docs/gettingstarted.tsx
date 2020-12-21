c8738d97f6e82e073eb03d7dc300031b30ff1889
Sonny Lazuardi Hermawan <sonny.lazuardi@grabtaxi.com>
2020-12-21 18:13:13 +0800

    Added codesandbox embed

--- src/pages/docs/gettingstarted.tsx
+++ src/pages/docs/gettingstarted.tsx
@@ -74,6 +74,20 @@ return (
             fontSize: 12,
           }}
         />
+
+        <iframe
+          src="https://codesandbox.io/embed/sonwan-ui-hl4yh?fontsize=14&hidenavigation=1&theme=dark"
+          style={{
+            width: "100%",
+            height: "500px",
+            border: 0,
+            borderRadius: 4,
+            overflow: "hidden",
+          }}
+          title="SonWan-UI"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
       </div>
     </Content>
   );
