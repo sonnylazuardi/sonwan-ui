a03751dd006f437aff2d5e64d293e7b43646524c
Sonny Lazuardi Hermawan <sonny.lazuardi@grabtaxi.com>
2020-12-21 19:10:11 +0800

    Center doc page container

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -28,62 +28,64 @@ const menu = [
 const DemoIndex = () => {
   const [active, setActive] = React.useState("");
   return (
-    <>
-      <div className="grid grid-cols-12">
-        <div className="col-span-full md:col-span-3 lg:col-span-2 relative">
-          <div className="sticky top-0 left-0 bottom-0 right-0 sm:h-screen flex flex-col text-black dark:text-white bg-light-100 dark:bg-dark-900 overflow-auto">
-            <Link
-              to="/"
-              className="text-2xl font-extrabold text-center tracking-tighter my-10"
-            >
-              SonWan UI
-            </Link>
-            {menu.map((item, i) => {
-              return (
-                <a
-                  key={i}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    scrollTo(e.currentTarget.getAttribute("href"));
-                    setActive(item.id);
+    <div className="bg-white dark:bg-dark-500">
+      <div className="max-w-screen-lg container mx-auto">
+        <div className="flex">
+          <div className="w-60 relative">
+            <div className="sticky top-0 left-0 bottom-0 right-0 sm:h-screen flex flex-col text-black dark:text-white bg-white dark:bg-dark-500 overflow-auto">
+              <Link
+                to="/"
+                className="text-2xl font-extrabold text-center tracking-tighter my-10"
+              >
+                SonWan UI
+              </Link>
+              {menu.map((item, i) => {
+                return (
+                  <a
+                    key={i}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      scrollTo(e.currentTarget.getAttribute("href"));
+                      setActive(item.id);
+                    }}
+                    href={`#${item.id}`}
+                    className={cn("p-4 mx-4 rounded-xl text-sm font-semibold", {
+                      "bg-light-100 dark:bg-dark-900 text-black dark:text-white":
+                        item.id === active,
+                      "text-gray-500 dark:text-light-100": item.id !== active,
+                    })}
+                  >
+                    {item.label}
+                  </a>
+                );
+              })}
+              <div className="p-4 px-8 mb-4">
+                <Switch
+                  name="sidebar"
+                  onChange={(darkMode) => {
+                    if (darkMode) {
+                      document.querySelector("html").classList.add("dark");
+                    } else {
+                      document.querySelector("html").classList.remove("dark");
+                    }
                   }}
-                  href={`#${item.id}`}
-                  className={cn("p-4 mx-4 rounded-xl text-sm font-semibold", {
-                    "bg-white dark:bg-dark-500 text-black dark:text-white":
-                      item.id === active,
-                    "text-gray-500 dark:text-light-100": item.id !== active,
-                  })}
-                >
-                  {item.label}
-                </a>
-              );
-            })}
-            <div className="p-4 px-8 mb-4">
-              <Switch
-                name="sidebar"
-                onChange={(darkMode) => {
-                  if (darkMode) {
-                    document.querySelector("html").classList.add("dark");
-                  } else {
-                    document.querySelector("html").classList.remove("dark");
-                  }
-                }}
-              />
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-span-full md:col-span-9 lg:col-span-10">
-          <GettingStarted />
-          <CardPage />
-          <CardListPage />
-          <SwitchPage />
-          <SwipePage />
-          <InputPage />
-          <CardCheckPage />
-          <IconsPage />
+          <div className="flex-1">
+            <GettingStarted />
+            <CardPage />
+            <CardListPage />
+            <SwitchPage />
+            <SwipePage />
+            <InputPage />
+            <CardCheckPage />
+            <IconsPage />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
